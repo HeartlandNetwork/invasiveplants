d8761ce476b6e652fb9a6908b8c527b005234dd7
HeartlandNetwork <gareth_rowell@nps.gov>
2024-09-25 16:27:58 -0500

    join issues

--- fixing_spatial/spatial_updates3.py
+++ fixing_spatial/spatial_updates3.py
@@ -3,7 +3,7 @@
 ###############################################################
 #
 #   spatial_updates3.py
-#   importing new thru 2023 
+#   importing new thru 2022
 #   and fixing spatial coordinates
 #   in invasive_plants data package
 #
@@ -42,76 +42,28 @@ df_wicr = pd.read_csv("WicrInpCenterPts.csv")
 
 # concatenate all 
 
-df_all = pd.concat([df_arpo, df_cuva, df_efmo, df_gwca], ignore_index=True)
-df_all = pd.concat([df_all, df_heho, df_hocu, df_home, df_hosp], ignore_index=True)
-df_all = pd.concat([df_all, df_libo, df_peri, df_pipe, df_tapr, df_wicr], ignore_index=True)
+df_spatial = pd.concat([df_arpo, df_cuva, df_efmo, df_gwca], ignore_index=True)
+df_spatial = pd.concat([df_spatial, df_heho, df_hocu, df_home, df_hosp], ignore_index=True)
+df_spatial = pd.concat([df_spatial, df_libo, df_peri, df_pipe, df_tapr, df_wicr], ignore_index=True)
 
-df_all.to_csv("df_all.csv", sep=',', encoding='utf-8')
+# load field data
 
-
-
-
+df_data = pd.read_csv("qryexp_FieldData.csv")
 
 
+# Perform an inner join - need inner join to omit any mismatches
 
+df_all = df_spatial.merge(df_data, on='LocationID', how='inner')
 
+# include sample year
 
+df_all['SampleYear'] = pd.to_numeric(df_all['PeriodID'].str[10:14])
 
+# write to file
 
-
-
-
-
-
-
-LatitudeInDecimalDegrees
-
-df["LatitudeInDecimalDegrees"].plot.hist
-
-# 
-# select columns
-
-df_pub = df_pub[['ParkName','ParkCode','LocationID','PeriodID', \
-    'CoverClassInMetersSquared','CommonName','ScientificName', \
-    'Date','Type','BasisofRecord','TaxonRank']]  
-df_hosp = df_hosp[['InpID','Acres','LocationID','Lon_DD', 'Lat_DD']]
-df_home = df_home[['InpID','Acres','LocationID','Lon_DD', 'Lat_DD']]
-
-# df_hosp LocationID is incomplete
-# LocationID is an integer
-
-df_hosp[['LocationID']] = df_hosp[['LocationID']].astype(str)
-df_hosp[['LocationID']] = 'HOSP_InvPla_' + df_hosp[['LocationID']]
-
-  # display columns
-
-print("pub ---------------------------------")
-print(df_pub.head(3))
-print("hosp ---------------------------------")
-print(df_hosp.head(3))
-print("home ---------------------------------")
-print(df_home.head(3))
-print("---------------------------------")
-
-
-# this script is fixing hosp and home
-# complete this and test in Power BI
-# if its good, reload published csv and filter out old
-# home and hosp and insert new home and hosp
-
-# left join df_pub on df_hosp and df_  
-
-result = df1.merge(df2, on='team', how='left')
+df_all.to_csv("invasive_plants_thru2022.csv", sep=',', encoding='utf-8')
 
 
 
-    
-
-
-
-# Select the 'Name' and 'City' columns
-selected_columns = df[['Name', 'City']]
-
-print(selected_columns)
 
 
